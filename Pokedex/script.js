580c37d3fc4060236b190630423d0233259d32de
Daniel Sánchez Cantalejo <daniel.sanchezc@eviden.com>
2024-01-31 16:36:39 +0100

    pokedex editada

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -3,8 +3,9 @@ const pokemonNumber = document.querySelector(".pokemon-number");
 const pokemonImage = document.querySelector(".pokemon-img");
 const form = document.querySelector(".form");
 const input = document.querySelector(".input-search");
-const buttonPrev = document.querySelector('.btn-prev')
-const buttonNext = document.querySelector('.btn-next')
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+const pokemonStats = document.getElementById("pokemon-stats");
 
 let searchPokemon = 258;
 
@@ -12,24 +13,35 @@ const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
   
   if(APIResponse.status === 200) { 
-  const data = await APIResponse.json();
-  return data;
+    const data = await APIResponse.json();
+    return data;
   }
 }
 
 const renderPokemon = async (pokemon) => {
-  
   pokemonName.innerHTML = 'Loading ...';
   
   const data = await fetchPokemon(pokemon);
   
   if (data) { 
-  pokemonImage.style.display = 'block';
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
-  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-  input.value = '';
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = `ID: ${data.id}`;
+    pokemonImage.src = data.sprites.front_default;
+    input.value = '';
     searchPokemon = data.id;
+
+    // Mostrar estadísticas
+    pokemonStats.innerHTML = `
+      <p>Weight: ${data.weight}</p>
+      <p>Height: ${data.height}</p>
+      <p>HP: ${data.stats[0].base_stat}</p>
+      <p>Attack: ${data.stats[1].base_stat}</p>
+      <p>Defense: ${data.stats[2].base_stat}</p>
+      <p>Special Attack: ${data.stats[3].base_stat}</p>
+      <p>Special Defense: ${data.stats[4].base_stat}</p>
+      <p>Speed: ${data.stats[5].base_stat}</p>
+    `;
   } else {
     pokemonImage.style.display = 'none';
     pokemonName.innerHTML = 'Not Found :c';
@@ -42,16 +54,4 @@ form.addEventListener('submit', (event) => {
   renderPokemon(input.value.toLowerCase());
 });
 
-buttonPrev.addEventListener('click', () => {
-  if (searchPokemon > 1) {
-  searchPokemon -= 1;
-  renderPokemon(searchPokemon);
-  }
-});
-
-buttonNext.addEventListener('click', () => {
-  searchPokemon += 1;
-  renderPokemon(searchPokemon);
-});
-
-renderPokemon(searchPokemon)
+renderPokemon(searchPokemon);
